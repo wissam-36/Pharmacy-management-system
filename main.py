b2d5430b2ce9dc6c60baaa1a45f9b60f4e4ef08d
wissam-36 <wissam.dev@proton.me>
2024-03-15 19:46:23 +0300

    Add mainscreen window

--- main.py
+++ main.py
@@ -2,16 +2,58 @@ from customtkinter import *
 from PIL import Image
 import os
 
+
+
+
+
 root = CTk()
 root.geometry('1080x720')
 root.title('Pharmacy management system')
 root.resizable(False, False)
 
 
-
+def clicked():
+    print('Done')
 
 def login_to_main_win ():
-    pass
+    background_login_label.pack_forget()
+    ent_username.place_forget()
+    ent_password.place_forget()
+    login_button.place_forget()
+    signup_label.place_forget()
+    signup_button.place_forget()
+
+
+    main_screen_background = CTkImage(Image.open(os.path.join("Pharmacy assets", "backgrounds", "mainscreen-01.jpg")), size=(1080, 720))
+    background_screen_label = CTkLabel(root, image=main_screen_background, text='')
+    background_screen_label.pack()
+
+    plus_image= CTkImage(Image.open(os.path.join("Pharmacy assets", "buttons", "plus.png")), size=(24, 24))
+    add_button = CTkButton(root, text='Add Medicine', bg_color='#F7F9F8', fg_color='#2157C2', text_color='white',height=47,width=195,corner_radius=10,hover_color='#3F70D4',image=plus_image,font=fnt_not_bold,command=clicked)
+    add_button.place(x=15,y=300)
+
+
+    drug_image= CTkImage(Image.open(os.path.join("Pharmacy assets", "buttons", "drug.png")), size=(24, 24))
+    view_button = CTkButton(root, text='View Medicine', bg_color='#F7F9F8', fg_color='#2157C2', text_color='white',height=47,width=195,corner_radius=10,hover_color='#3F70D4',image=drug_image,font=fnt_not_bold,command=clicked)
+    view_button.place(x=15,y=360)
+
+
+
+
+    drug_image= CTkImage(Image.open(os.path.join("Pharmacy assets", "buttons", "pills.png")), size=(24, 24))
+    modify_button = CTkButton(root, text='Modify Medicine', bg_color='#F7F9F8', fg_color='#2157C2', text_color='white',height=47,width=195,corner_radius=10,hover_color='#3F70D4',image=drug_image,font=fnt_not_bold,command=clicked)
+    modify_button.place(x=15,y=420)
+
+
+    drug_image= CTkImage(Image.open(os.path.join("Pharmacy assets", "buttons", "validity.png")), size=(24, 24))
+    modify_button = CTkButton(root, text='Modify Medicine', bg_color='#F7F9F8', fg_color='#2157C2', text_color='white',height=47,width=195,corner_radius=10,hover_color='#3F70D4',image=drug_image,font=fnt_not_bold,command=clicked)
+    modify_button.place(x=15,y=480)
+
+
+
+
+
+
 
 
 
@@ -66,7 +108,7 @@ def signup_win():
     ent_new_password = CTkEntry(root, placeholder_text='New Password', bg_color='#F7F9F8', fg_color='white', border_color='#1E61D8', corner_radius=25, height=47, width=316, text_color='black')
     ent_new_password.place(x=40,y=380)
 
-    signup_btn = CTkButton(root, text='Sign up', text_color='white', bg_color='#F7F9F8', fg_color='#2658B6', width=315, height=53, corner_radius=25, font=fnt,command=sign_up)
+    signup_btn = CTkButton(root, text='Sign up', text_color='white', bg_color='#F7F9F8', fg_color='#2658B6', width=315, height=53, corner_radius=25, font=fnt,command=sign_up,hover_color='#3F70D4')
     signup_btn.place(x=40,y=490)
 
     back_btn = CTkButton(root, text='<', text_color='#2658B6', bg_color='#F7F9F8', fg_color='#F7F9F8',hover_color='#F7F9F8',height=25,width=25,font=CTkFont(family='Bahnschrift', size=30), command=back_to_login)
@@ -80,14 +122,14 @@ background_login_label = CTkLabel(root, image=login_background, text='')
 background_login_label.pack()
 
 fnt = CTkFont(family='Bahnschrift', weight='bold', size=20)
-
+fnt_not_bold = CTkFont(family='Bahnschrift', size=17)
 ent_username = CTkEntry(root, placeholder_text='Username', bg_color='#F7F9F8', fg_color='white', border_color='#1E61D8', corner_radius=25, height=47, width=316, text_color='black')
 ent_username.place(x=40,y=300)
 
 ent_password = CTkEntry(root, placeholder_text='Password', bg_color='#F7F9F8', fg_color='white', border_color='#1E61D8', corner_radius=25, height=47, width=316, text_color='black')
 ent_password.place(x=40,y=380)
 
-login_button = CTkButton(root, text='Login', text_color='white', bg_color='#F7F9F8', fg_color='#2658B6', width=315, height=53, corner_radius=25, font=fnt,command=login_to_main_win)
+login_button = CTkButton(root, text='Login', text_color='white', bg_color='#F7F9F8', fg_color='#2658B6', width=315, height=53, corner_radius=25, font=fnt,command=login_to_main_win,hover_color='#3F70D4')
 login_button.place(x=40,y=490)
 
 signup_label = CTkLabel(root, text='Dont have an account ?', bg_color='#F7F9F8', text_color='black')

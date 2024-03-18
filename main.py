8091210307a4c2fa95436494d32002f938ec94aa
wissam-36 <wissam.dev@proton.me>
2024-03-18 23:25:38 +0300

    Add view_medicines function

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from customtkinter import *
 from tkinter import messagebox
+from customtkinter import CTkToplevel
+from tkinter import Text,Toplevel
 from PIL import Image,ImageTk
 import os
 import mysql.connector
+import tkinter as tk
+from tkinter import Text
+from tkinter import ttk
 
 
 
@@ -22,30 +27,107 @@ root.iconphoto(False, iconpath)
 
 
 
-
 def add_medicine():
+    def save_medicine():
+        if (medicine_name.get()=='' or medicine_name.get()=='Medicine Name') or \
+        (medicine_quantity.get()=='' or medicine_quantity.get()=='Quantity') or \
+        (validity_date.get()=='' or validity_date.get()=='Validity Date') or \
+        (purchase_price.get()=='' or purchase_price.get()=='purchase Price') or \
+        (selling_price.get()=='' or selling_price.get()=='Selling Price'):
+            messagebox.showerror('Entry error', 'There are entries that you did not write !!')
+        else:
+            try:
+                # Connect to the database
+                mydb = mysql.connector.connect(host='localhost', user='root', password='2003', database='PharmacyDB')
+                mycursor = mydb.cursor()
+                print("connected to database")
+            
+                # Insert new medicine into database
+                command = "INSERT INTO Medicines (medicine_name, expiry_date, purchase_price, selling_price, quantity) VALUES (%s, %s, %s, %s, %s)"
+                data = (medicine_name.get(), validity_date.get(), purchase_price.get(), selling_price.get(), medicine_quantity.get())
+                mycursor.execute(command, data)
+            
+                mydb.commit()
+                mydb.close()
+            
+                messagebox.showinfo("Success", "New Medicine added successfully")
 
-    medicine_name = CTkEntry(root, placeholder_text='Medicine Name', bg_color='#E7ECF2', fg_color='white', text_color='black',height=40,width=260,corner_radius=5)
-    medicine_name.place(x=240,y=40)
+            except mysql.connector.Error as err:
+                messagebox.showerror('Database Error', f'Database error: {err}')
 
 
-    medicine_quantity = CTkEntry(root, placeholder_text='Quantity', bg_color='#E7ECF2', fg_color='white', text_color='black',height=40,width=260,corner_radius=5)
-    medicine_quantity.place(x=600,y=40)
+    medicine_name = CTkEntry(root, placeholder_text='Medicine Name', bg_color='#E7ECF2', fg_color='white',
+                             text_color='black', height=40, width=260, corner_radius=5)
+    medicine_name.place(x=240, y=40)
+
+    medicine_quantity = CTkEntry(root, placeholder_text='Quantity', bg_color='#E7ECF2', fg_color='white',
+                                 text_color='black', height=40, width=260, corner_radius=5)
+    medicine_quantity.place(x=600, y=40)
+
+    validity_date = CTkEntry(root, placeholder_text='Example 2025-7-18', bg_color='#E7ECF2', fg_color='white',
+                              text_color='black', height=40, width=260, corner_radius=5)
+    validity_date.place(x=240, y=160)
+
+    purchase_price = CTkEntry(root, placeholder_text='purchase Price', bg_color='#E7ECF2', fg_color='white',
+                             text_color='black', height=40, width=260, corner_radius=5)
+    purchase_price.place(x=600, y=160)
+
+    selling_price = CTkEntry(root, placeholder_text='Selling Price', bg_color='#E7ECF2', fg_color='white',
+                             text_color='black', height=40, width=260, corner_radius=5)
+    selling_price.place(x=600, y=200)
+
+    done_btn = CTkButton(root, text='Done', bg_color='#E7ECF2', fg_color='#3F70D4', text_color='white',
+                         corner_radius=5, hover_color='#618EE0', font=fnt_not_bold, command=save_medicine)
+    done_btn.place(x=900, y=600)
+
+    reset_btn = CTkButton(root, text='Reset', bg_color='#E7ECF2', fg_color='#3F70D4', text_color='white',
+                          corner_radius=5, hover_color='#618EE0', font=fnt_not_bold)
+    reset_btn.place(x=900, y=640)
 
 
-    validity_date = CTkEntry(root, placeholder_text='Validity Date', bg_color='#E7ECF2', fg_color='white', text_color='black',height=40,width=260,corner_radius=5)
-    validity_date.place(x=240,y=160)
 
 
-    selling_price = CTkEntry(root, placeholder_text='Selling Price', bg_color='#E7ECF2', fg_color='white', text_color='black',height=40,width=260,corner_radius=5)
-    selling_price.place(x=600,y=160)
 
+def view_medicines():
+    
+    try:
+        # Connect to the database
+        mydb = mysql.connector.connect(host='localhost', user='root', password='2003', database='PharmacyDB')
+        mycursor = mydb.cursor()
 
-    done_btn =  CTkButton(root, text='Done',bg_color='#E7ECF2', fg_color='#3F70D4', text_color='white',corner_radius=5,hover_color='#618EE0',font=fnt_not_bold)
-    done_btn.place(x=900,y=600)
+        # Execute the query to fetch data
+        mycursor.execute("SELECT medicine_name, expiry_date, purchase_price, selling_price, quantity FROM Medicines")
+        medicines = mycursor.fetchall()
 
-    reset_btn =  CTkButton(root, text='Reset',bg_color='#E7ECF2', fg_color='#3F70D4', text_color='white',corner_radius=5,hover_color='#618EE0',font=fnt_not_bold)
-    reset_btn.place(x=900,y=640)
+        # Create a frame to contain the treeview
+        tree_frame = CTkFrame(root)
+        tree_frame.place(x=330, y=40)
+
+        # Define treeview
+        tree = ttk.Treeview(tree_frame, columns=("Medicine Name", "Expiry Date", "Purchase Price", "Selling Price", "Quantity"))
+        tree.pack(expand=True, fill="both")
+
+        # Define column headings and adjust column width
+        tree.heading("#0", text="ID", anchor=tk.CENTER)
+        tree.column("#0", width=50)
+        tree.heading("#1", text="Medicine Name", anchor=tk.CENTER)
+        tree.column("#1", width=150)
+        tree.heading("#2", text="Expiry Date", anchor=tk.CENTER)
+        tree.column("#2", width=100)
+        tree.heading("#3", text="Purchase Price", anchor=tk.CENTER)
+        tree.column("#3", width=100)
+        tree.heading("#4", text="Selling Price", anchor=tk.CENTER)
+        tree.column("#4", width=100)
+        tree.heading("#5", text="Quantity", anchor=tk.CENTER)
+        tree.column("#5", width=100)
+
+        # Insert data into the treeview
+        for idx, medicine in enumerate(medicines, start=1):
+            tree.insert("", "end", text=idx, values=medicine)
+
+        mydb.close()
+    except mysql.connector.Error as err:
+        messagebox.showerror('Database Error', f'Database error: {err}')
 
 
 # Function to handle the login process
@@ -95,7 +177,7 @@ def main_window():
     add_button.place(x=10, y=300)
 
     drug_image= CTkImage(Image.open(os.path.join("Pharmacy assets", "buttons", "drug.png")), size=(24, 24))
-    view_button = CTkButton(root, text='View Medicine',bg_color='#2157C2', fg_color='#3F70D4', text_color='white',height=47,width=195,corner_radius=10,hover_color='#618EE0',image=drug_image,font=fnt_not_bold,command=add_medicine)
+    view_button = CTkButton(root, text='View Medicine',bg_color='#2157C2', fg_color='#3F70D4', text_color='white',height=47,width=195,corner_radius=10,hover_color='#618EE0',image=drug_image,font=fnt_not_bold,command=view_medicines)
     view_button.place(x=10,y=360)
 
 
